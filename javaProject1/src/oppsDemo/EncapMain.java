babeb6cb6f278053c4762e08bf494b3a9b3aa683
Lakshmi N <161922984+lakshu2702@users.noreply.github.com>
2025-03-31 07:32:45 +0530

    Fixing the errors

--- javaProject1/src/oppsDemo/EncapMain.java
+++ javaProject1/src/oppsDemo/EncapMain.java
@@ -2,4 +2,15 @@ package oppsDemo;
 
 public class EncapMain {
 
+	public static void main(String[] args) {
+		
+		// Object creation
+		EncapDemo obj = new EncapDemo(); // new is keywords which is used to create a object
+		obj.setSerialNum(101);
+		obj.setName("Bunny");
+		obj.setAge(15);
+		System.out.println(obj);
+		
+	}
+
 }
