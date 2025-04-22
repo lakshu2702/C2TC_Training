c8f6d60f52c37a3e2715e84e6f188782b01f6117
Lakshmi N <161922984+lakshu2702@users.noreply.github.com>
2025-04-22 07:39:35 +0530

    Updating notes

--- Class_Notes/src/exceptionHandling/UncheckedException.java
+++ Class_Notes/src/exceptionHandling/UncheckedException.java
@@ -4,7 +4,7 @@ public class UncheckedException {
 
 	public static void main(String[] args) {
 		int[] arr = new int[3];
-		System,out.println(arr[5]);
+		System.out.println(arr[5]);
 	
 
 	}
---
+++ Class_Notes/src/non_access/demo.java
@@ -0,0 +1,12 @@
+package non_access;
+
+public class demo {
+	public static void main(String[] args) {
+		employee ob= new employee("JHANAVI",2);
+		System.out.println(ob);
+		employee companyName;
+		ob=new employee("Qamreen",3);
+		System.out.println(ob);
+	}
+
+}
